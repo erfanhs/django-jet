8860b0068322946536bcd28521b4d957da918e85
Erfan Harirsaz <erfanharirsaz071@gmail.com>
2023-09-18 23:42:18 -0700

    Update utils.py

--- jet/utils.py
+++ jet/utils.py
@@ -23,11 +23,11 @@ from django.contrib.admin import AdminSite
 from django.utils.encoding import smart_str as smart_text
 from django.utils.text import capfirst
 from django.contrib import messages
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str as force_text
 from django.utils.functional import Promise
 from django.contrib.admin.options import IncorrectLookupParameters
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
 
 try:
