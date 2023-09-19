04dcc7130d01db449dd498f5a04b6c0f5802033a
Erfan Harirsaz <erfanharirsaz071@gmail.com>
2023-09-18 23:39:25 -0700

    Update urls.py

--- jet/urls.py
+++ jet/urls.py
@@ -1,5 +1,5 @@
 import django
-from django.conf.urls import url
+from django.urls import path
 
 try:
     from django.views.i18n import JavaScriptCatalog
@@ -13,28 +13,28 @@ from jet.views import add_bookmark_view, remove_bookmark_view, toggle_applicatio
 app_name = 'jet'
 
 urlpatterns = [
-    url(
-        r'^add_bookmark/$',
+    path(
+        r'add_bookmark/',
         add_bookmark_view,
         name='add_bookmark'
     ),
-    url(
-        r'^remove_bookmark/$',
+    path(
+        r'remove_bookmark/',
         remove_bookmark_view,
         name='remove_bookmark'
     ),
-    url(
-        r'^toggle_application_pin/$',
+    path(
+        r'toggle_application_pin/',
         toggle_application_pin_view,
         name='toggle_application_pin'
     ),
-    url(
-        r'^model_lookup/$',
+    path(
+        r'model_lookup/',
         model_lookup_view,
         name='model_lookup'
     ),
-    url(
-        r'^jsi18n/$',
+    path(
+        r'jsi18n/',
         javascript_catalog,
         {'packages': 'django.contrib.admin+jet'},
         name='jsi18n'
